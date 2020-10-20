88561733fe21a6adbbffb9998b5d5ef5ee71ead5
Andreas Dilaveris <projects@thelondonmedialab.com>
2020-10-20 19:01:58 +0100

    add notest to webpack-server.js

--- server/webpack-server.js
+++ server/webpack-server.js
@@ -1,26 +1,48 @@
 #!/usr/bin/env node
 
-const   { parsed: { webpack_port: port }} = require('dotenv').config() ;
+const {
+  parsed: {
+    webpack_port: port
+  }
+} = require('dotenv').config();
 
+const http = require("http")
 const express = require("express");
 const app = express();
 
+const server = http.createServer(app);
+
 /* 
+  use webpack-dev-server through the Node.js API
   generate compiler using webpack(config) 
 */
 
 const mode = process.env.MODE || 'development';
 
 const webpack = require("webpack");
-const config = require("../webpack.config.js")({}, { mode });
+/* functional config */
+const config = require("../webpack.config.js")({}, {
+  mode
+});
 const compiler = webpack(config);
 
-/* 
-  middleware 
+/* middleware */
+/*
+    const devServerOptions = Object.assign({}, config.devServer, {
+      open: true,
+      stats: {
+        colors: true,
+      },
+    });
 */
 
 const webpackDev = require("webpack-dev-middleware")(compiler, config.devServer);
 const webpackHot = require("webpack-hot-middleware")(compiler);
+
+// /*
+//   https://github.com/60frames/webpack-hot-server-middleware
+// */
+
 app.use(webpackDev);
 app.use(webpackHot);
 
@@ -28,12 +50,12 @@ app.use(express.static("dist"));
 app.listen(`${port}`, () => console.log(`port ${port}`));
 
 
-
 /*
 
   two ways for webpack-dev-server hot update:
 
   1  webpack-dev-server, hot=true, inline=true
+
   2  express server: webpack-dev-middleware + webpack-hot-middleware, __webpack_hmr
 
         const compiler = webpack(webpackConfig);
